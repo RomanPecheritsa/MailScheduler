478437af89283d1650b4718d1f4420234b684c22
pecheritsa <pecheritsa.roman@gmail.com>
2024-10-18 21:06:36 +0300

    add path users

--- mailscheduler/urls.py
+++ mailscheduler/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("mailing.urls")),
+    path("users/", include("users.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
