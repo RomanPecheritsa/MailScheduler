b663f095bb3ec1501dd25af8a808992105a6fd3a
pecheritsa <pecheritsa.roman@gmail.com>
2024-10-18 21:04:43 +0300

    add mixin StyleFormMixin

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,32 +1,25 @@
 from django import forms
-from mailing.models import Client, Message, Mailing, MailingAttempt
+
+from mailing.models import Client, Message, Mailing
+from mailing.mixins import StyleFormMixin as SFM
 
 
-class ClientForm(forms.ModelForm):
+class ClientForm(SFM, forms.ModelForm):
     class Meta:
         model = Client
         fields = ["full_name", "email", "comment"]
-        widgets = {
-            "full_name": forms.TextInput(attrs={"class": "validate"}),
-            "email": forms.EmailInput(attrs={"class": "validate"}),
-            "comment": forms.Textarea(attrs={"class": "materialize-textarea"}),
-        }
 
 
-class MessageForm(forms.ModelForm):
+class MessageForm(SFM, forms.ModelForm):
     class Meta:
         model = Message
         fields = ["title", "body"]
-        widgets = {
-            "title": forms.TextInput(attrs={"class": "input-field"}),
-            "body": forms.Textarea(attrs={"class": "materialize-textarea"}),
-        }
 
 
-class MailingForm(forms.ModelForm):
+class MailingForm(SFM, forms.ModelForm):
     send_time = forms.DateTimeField(
         widget=forms.DateTimeInput(
-            attrs={"type": "datetime-local", "class": "validate"},
+            attrs={"type": "datetime-local"},
             format="%Y-%m-%dT%H:%M",
         ),
         input_formats=["%Y-%m-%dT%H:%M"],
@@ -41,17 +34,4 @@ class MailingForm(forms.ModelForm):
             "message",
             "clients",
         ]
-        widgets = {
-            "frequency": forms.Select(attrs={"class": "input-field"}),
-            "message": forms.Select(attrs={"class": "input-field"}),
-            "clients": forms.SelectMultiple(attrs={"class": "input-field"}),
-        }
 
-    def __init__(self, *args, **kwargs):
-        super(MailingForm, self).__init__(*args, **kwargs)
-        if self.instance and self.instance.send_time:
-            self.initial["send_time"] = self.instance.send_time.strftime(
-                "%Y-%m-%dT%H:%M"
-            )
-        else:
-            self.instance.status = Mailing.Status.CREATED
