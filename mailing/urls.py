ced875f8ac26b7ec738c0e11a154ee59a33f7a99
pecheritsa <pecheritsa.roman@gmail.com>
2024-10-18 12:17:40 +0300

    restructure module, linter

--- mailing/urls.py
+++ mailing/urls.py
@@ -17,21 +17,21 @@ from mailing.views import (
     MailingUpdateView,
     MailingDeleteView,
     MailingToggleActiveView,
-    MailingAttemptListView
+    MailingAttemptListView,
 )
 
 app_name = "mailing"
 
 clients_patterns = [
-    path('', ClientListView.as_view(), name="client_list"),
-    path('<int:pk>/', ClientDetailView.as_view(), name="client_detail"),
-    path('create/', ClientCreateView.as_view(), name="client_create"),
+    path("", ClientListView.as_view(), name="client_list"),
+    path("<int:pk>/", ClientDetailView.as_view(), name="client_detail"),
+    path("create/", ClientCreateView.as_view(), name="client_create"),
     path("edit/<int:pk>/", ClientUpdateView.as_view(), name="client_edit"),
-    path("delete/<int:pk>/", ClientDeleteView.as_view(), name="client_delete")
+    path("delete/<int:pk>/", ClientDeleteView.as_view(), name="client_delete"),
 ]
 
 messages_patterns = [
-    path('', MessageListView.as_view(), name="message_list"),
+    path("", MessageListView.as_view(), name="message_list"),
     path("<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
     path("create/", MessageCreateView.as_view(), name="message_create"),
     path("edit/<int:pk>/", MessageUpdateView.as_view(), name="message_edit"),
@@ -44,11 +44,11 @@ mailings_patterns = [
     path("create/", MailingCreateView.as_view(), name="mailing_create"),
     path("edit/<int:pk>/", MailingUpdateView.as_view(), name="mailing_edit"),
     path("delete/<int:pk>/", MailingDeleteView.as_view(), name="mailing_delete"),
-    path("toggle-active/<int:pk>/", MailingToggleActiveView.as_view(), name='mailing_toggle_active'),
+    path("toggle-active/<int:pk>/", MailingToggleActiveView.as_view(), name="mailing_toggle_active"),
 ]
 
 attempts_patterns = [
-    path('', MailingAttemptListView.as_view(), name='mailing_attempt_list'),
+    path("", MailingAttemptListView.as_view(), name="mailing_attempt_list"),
 ]
 
 urlpatterns = [
@@ -56,5 +56,5 @@ urlpatterns = [
     path("clients/", include(clients_patterns)),
     path("messages/", include(messages_patterns)),
     path("mailings/", include(mailings_patterns)),
-    path("attempts/", include(attempts_patterns))
+    path("attempts/", include(attempts_patterns)),
 ]
