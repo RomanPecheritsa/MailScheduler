7acad18796ca46f5e44ea92e94e725429209622d
pecheritsa <pecheritsa.roman@gmail.com>
2024-10-28 14:29:18 +0300

    linter module

--- users/forms.py
+++ users/forms.py
@@ -6,34 +6,26 @@ from users.models import User
 
 class UserAuthenticationForm(AuthenticationForm):
     username = forms.EmailField(
-        label='Email',
-        widget=forms.EmailInput(attrs={"class": "validate"})
+        label="Email", widget=forms.EmailInput(attrs={"class": "validate"})
     )
     password = forms.CharField(
-        label='Пароль',
-        widget=forms.PasswordInput(attrs={"class": "validate"})
+        label="Пароль", widget=forms.PasswordInput(attrs={"class": "validate"})
     )
 
 
 class UserCreateForm(UserCreationForm):
     email = forms.EmailField(
         label="Email",
-        widget=forms.EmailInput(
-            attrs={"class": "validate"}
-        ),
+        widget=forms.EmailInput(attrs={"class": "validate"}),
     )
 
     password1 = forms.CharField(
         label="Пароль",
-        widget=forms.PasswordInput(
-            attrs={"class": "validate"}
-        ),
+        widget=forms.PasswordInput(attrs={"class": "validate"}),
     )
     password2 = forms.CharField(
         label="Подтверждение пароля",
-        widget=forms.PasswordInput(
-            attrs={"class": "validate"}
-        ),
+        widget=forms.PasswordInput(attrs={"class": "validate"}),
     )
 
     class Meta:
