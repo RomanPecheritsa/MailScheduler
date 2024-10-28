d3b13919c795ba55139b16728b88bf0bf463d3dc
pecheritsa <pecheritsa.roman@gmail.com>
2024-10-28 14:22:45 +0300

    changed models, add owner to message, client, mailing, linter module

--- mailing/models.py
+++ mailing/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from mailing.utils import upload_to, NULLABLE
+from users.models import User
+
+BLANK_NULL_TRUE = {"blank": True, "null": True}
 
 
 class Client(models.Model):
     """Service client"""
 
-    email = models.EmailField(unique=True, verbose_name="почта")
+    email = models.EmailField(verbose_name="почта")
     full_name = models.CharField(max_length=150, verbose_name="ФИО")
     comment = models.TextField(**NULLABLE, verbose_name="комментарий")
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        **BLANK_NULL_TRUE,
+        verbose_name="владелец клиента",
+    )
 
     def __str__(self):
         return f"{self.full_name} ({self.email})"
@@ -23,6 +32,12 @@ class Message(models.Model):
 
     title = models.CharField(max_length=100, verbose_name="тема письма")
     body = models.TextField(verbose_name="текст письма")
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        **BLANK_NULL_TRUE,
+        verbose_name="владелец сообщения",
+    )
 
     def __str__(self):
         return self.title
@@ -50,9 +65,7 @@ class Mailing(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True, verbose_name="дата и время создания рассылки"
     )
-    send_time = models.DateTimeField(
-        verbose_name="дата и время первой отправки рассылки"
-    )
+    send_time = models.DateTimeField(verbose_name="дата и время отправки рассылки")
     frequency = models.CharField(
         max_length=1,
         choices=Frequency.choices,
@@ -74,6 +87,12 @@ class Mailing(models.Model):
     clients = models.ManyToManyField(
         Client, related_name="clients", verbose_name="клиенты"
     )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        **BLANK_NULL_TRUE,
+        verbose_name="владелец рассылки",
+    )
 
     def __str__(self):
         return f"Рассылка: {self.id}, Статус: {self.status}"
@@ -82,6 +101,9 @@ class Mailing(models.Model):
         verbose_name = "рассылка"
         verbose_name_plural = "рассылки"
         ordering = ("-created_at",)
+        permissions = [
+            ("disable_mailing", "Может делать рассылку неативной"),
+        ]
 
 
 class MailingAttempt(models.Model):
